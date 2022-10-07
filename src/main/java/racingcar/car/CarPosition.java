f6109e088deb19334df54960b0f83b8dbc832a9d
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-10-07 18:42:20 +0900

    test(CarPosition.java) : 자동차의 처음 위치는 0이다.

---
+++ src/main/java/racingcar/car/CarPosition.java
@@ -0,0 +1,30 @@
+package racingcar.car;
+
+import java.util.Objects;
+
+public class CarPosition {
+
+    private final int position;
+
+    protected CarPosition() {
+        this.position = 0;
+    }
+
+    public CarPosition(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarPosition that = (CarPosition) o;
+        return this.position == that.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.position);
+    }
+
+}
---
+++ src/test/java/racingcar/car/CarPositionTest.java
@@ -0,0 +1,17 @@
+package racingcar.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarPositionTest {
+
+    @DisplayName("자동차 위치의 초기값은 0이다.")
+    @Test
+    void createCarPosition() {
+        CarPosition carPosition = new CarPosition();
+        assertThat(carPosition).isEqualTo(new CarPosition(0));
+    }
+
+}
