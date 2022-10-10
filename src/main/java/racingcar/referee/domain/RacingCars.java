360cd492dfe2335edeee10732f6e9ea7577def9f
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-10-10 12:32:22 +0900

    test(RacingCarsTest.java) : 같은 이름으로 경주 자동차 등록 불가

--- src/main/java/racingcar/referee/domain/RacingCars.java
+++ src/main/java/racingcar/referee/domain/RacingCars.java
@@ -11,7 +11,18 @@ public class RacingCars {
     List<Car> cars = new ArrayList<>();
 
     public void add(Car car) {
+        validate(car);
         this.cars.add(car);
+    }
+
+    private void validate(Car addCar) {
+        for (Car car : this.cars) {
+            if (addCar.carName().equals(car.carName())) {
+                System.out.println("[ERROR] 같은 이름의 자동차를 추가할 수 없습니다.");
+                System.out.println();
+                throw new IllegalArgumentException("[ERROR] 같은 이름의 자동차를 추가할 수 없습니다.");
+            }
+        }
     }
 
     public int size() {
