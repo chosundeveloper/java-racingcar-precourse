120aa7f8679c33a00217e5f533c1fdc0a99f2aad
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-10-10 13:51:42 +0900

    refactor(OutputPlayRound.java) : 메서드 인라인

--- src/main/java/racingcar/referee/ui/OutputPlayRound.java
+++ src/main/java/racingcar/referee/ui/OutputPlayRound.java
@@ -7,17 +7,16 @@ public class OutputPlayRound {
 
     public static void printPlayRound(RacingCars racingCars) {
         for (Car car : racingCars.cars()) {
-            int position = car.carPosition().position();
             StringBuilder content = new StringBuilder(car.carName() + " : ");
-            convertFromPositionToDash(position, content);
+            convertFromPositionToDash(car.carPosition().position(), content);
             System.out.println(content);
         }
         System.out.println();
     }
 
-    private static void convertFromPositionToDash(int position, StringBuilder print) {
+    private static void convertFromPositionToDash(int position, StringBuilder content) {
         for (int i = 0; i < position; i++) {
-            print.append("-");
+            content.append("-");
         }
     }
 }
